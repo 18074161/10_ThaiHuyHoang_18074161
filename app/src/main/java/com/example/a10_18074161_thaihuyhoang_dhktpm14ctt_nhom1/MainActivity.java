f4e38c8a6d200bd22b6492641208e7dbb8cca50e
18074161 <thaihuyhoang0610@gmail.com>
2021-12-20 18:47:54 +0700

    thaihuyhoang2k 1a

---
+++ app/src/main/java/com/example/a10_18074161_thaihuyhoang_dhktpm14ctt_nhom1/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.a10_18074161_thaihuyhoang_dhktpm14ctt_nhom1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private int RC_SIGN_IN = 0;
+    private GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+    List<User> mUsers = new ArrayList<>();
+
+    TextView tvReg;
+    ImageButton btnLogin;
+    EditText edtAccount, adtPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        btnLogin = findViewById(R.id.btnLogin);
+        edtAccount = findViewById(R.id.edtacc);
+        adtPass = findViewById(R.id.edtpass);
+        tvReg = findViewById(R.id.tvReg);
+
+        tvReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterMainActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getList();
+                String acc = edtAccount.getText().toString();
+                String pass = adtPass.getText().toString();
+                for (User user: mUsers
+                     ) {
+                 if(user.getEmail().equals(acc) && user.getPassword().equals(pass)){
+                     Intent intent=  new Intent(MainActivity.this, TransferMainActivity.class);
+                     Toast.makeText(MainActivity.this, "Dang nhap thanh cong", Toast.LENGTH_SHORT).show();
+                     startActivity(intent);
+                 }
+
+                }
+//                Toast.makeText(MainActivity.this, "tai khoan mat khau khong hop le", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void getList(){
+        ApiGmail.apiService.getUsers().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                mUsers = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+
+            }
+        });
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String personName = account.getDisplayName();
+            String personGivenName = account.getGivenName();
+            String personFamilyName = account.getFamilyName();
+            String personEmail = account.getEmail();
+            String personId = account.getId();
+            Uri personPhoto = account.getPhotoUrl();
+            Date date = new Date();
+
+            ApiGmail.apiService.addGmail(new Gmail(personName, personEmail, date+"")).enqueue(new Callback<Gmail>() {
+                @Override
+                public void onResponse(Call<Gmail> call, Response<Gmail> response) {
+                    Toast.makeText(MainActivity.this, "addSuccess gmail", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<Gmail> call, Throwable t) {
+
+                }
+            });
+//
+            startActivity(new Intent(MainActivity.this, TransferMainActivity.class));
+            // Signed in successfully, show authenticated UI.
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a10_18074161_thaihuyhoang_dhktpm14ctt_nhom1/TransferMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a10_18074161_thaihuyhoang_dhktpm14ctt_nhom1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class TransferMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_main);
+    }
+}
